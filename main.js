983e5e2d90e0f4a3ea681b7568d422d4d13ee3dd
toiletsplurge <MrTakeTooMuch@gmail.com>
2023-02-22 00:47:28 -0500

    Overhaul
    Changed the API since the last one ended its service. Changed the styles to display the articles as cards in a grid now. Got rid of the removeDuplicates function since the API doesn't produce duplicate articles.

--- main.js
+++ main.js
@@ -4,41 +4,50 @@ let articleURL = " ";
 
 const newsList = document.querySelector(".news-list");
 
-//api key
 const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'b9d9fa1b41msh1830115e5b81cf6p170c07jsnba165b6b0be1',
-		'X-RapidAPI-Host': 'environment-news-live.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "5b1b0b21cemsh840a941f6a256d2p174560jsnd8e9e49d728f",
+    "X-RapidAPI-Host": "climate-news-feed.p.rapidapi.com",
+  },
 };
 
-//removes duplicate articles
-const removeDuplicates = (arr, key) => {
-  return [...new Map(arr.map(item => [item[key], item])).values()]
-}
-
 const getData = () => {
-  fetch('https://environment-news-live.p.rapidapi.com/news/', options).then(response => {
-    return response.json();
-  }).then( data => {
-      console.log(data)
-      let uniqueArticles = removeDuplicates(data, "url")
-      uniqueArticles.forEach(article => {
-        let p = document.createElement("p");
-        let li = document.createElement("li");
-        let a = document.createElement("a");
-        let br = document.createElement("br");
-        p.textContent = article.title;
-        a.setAttribute("href", article.url);
-        a.setAttribute("target", "_blank");
-        a.textContent = "Link to Article";
-        li.appendChild(a);
-        newsList.appendChild(br);
-        newsList.appendChild(p);
-        newsList.appendChild(li);
-      })
-  }).catch(err => console.error(err));
-}
+  fetch(
+    "https://environment-news-live.p.rapidapi.com/?source=Nasa%20Climate&limit=50&exclude=The%20Guardian",
+    options
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      data.articles.forEach((article) => {
+        console.log(article.title);
+        console.log(article.url);
+        let card = document.createElement("div");
+        card.classList.add("card");
+        let img = document.createElement("img");
+        img.classList.add("card-img-top");
+        img.src = article.thumbnail;
+        let cardBody = document.createElement("div");
+        cardBody.classList.add("card-body");
+        let title = document.createElement("h5");
+        title.classList.add("card-title");
+        title.textContent = article.title;
+        let link = document.createElement("a");
+        link.classList.add("btn", "btn-primary");
+        link.setAttribute("href", article.url);
+        link.setAttribute("target", "_blank");
+        link.textContent = "Read More";
+        cardBody.appendChild(title);
+        cardBody.appendChild(link);
+        card.appendChild(img);
+        card.appendChild(cardBody);
+        newsList.appendChild(card);
+      });
+    })
+    .catch((err) => console.error(err));
+};
 
 getData();
